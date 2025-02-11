3f0d74eb3c227e29bb0594477a167f96cd1824c7
Chetan Pawar <chetanpawar2002official@gmail.com>
2025-02-11 11:17:07 +0530

    end to end project

---
+++ src/helper.py
@@ -0,0 +1,54 @@
+import os
+from git import Repo
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import LanguageParser
+from langchain.text_splitter import Language
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+load_dotenv()
+
+HF_TOKEN= os.getenv('HF_TOKEN')
+os.environ['HF_TOKEN']= HF_TOKEN
+
+
+#clone any github repositories 
+def repo_ingestion(repo_url):
+    os.makedirs("repo", exist_ok=True)
+    repo_path = "repo/"
+    Repo.clone_from(repo_url, to_path=repo_path)
+
+
+
+
+#Loading repositories as documents
+def load_repo(repo_path):
+    loader = GenericLoader.from_filesystem(repo_path,
+                                        glob = "**/*",
+                                       suffixes=[".py"],
+                                       parser = LanguageParser(language=Language.PYTHON, parser_threshold=500)
+                                        )
+    
+    documents = loader.load()
+
+    return documents
+
+
+
+
+#Creating text chunks 
+def text_splitter(documents):
+    documents_splitter = RecursiveCharacterTextSplitter.from_language(language = Language.PYTHON,
+                                                             chunk_size = 2000,
+                                                             chunk_overlap = 200)
+    
+    text_chunks = documents_splitter.split_documents(documents)
+
+    return text_chunks
+
+
+
+#loading embeddings model
+def load_embedding():
+    embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    return embeddings
